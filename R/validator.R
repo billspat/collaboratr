ec078294f0b886f6dc4d5b1bec27dcd1775fed2c
Pat Bills <billspat@msu.edu>
2024-08-14 14:00:45 -0400

    use validation rules file internal to package

--- R/validator.R
+++ R/validator.R
@@ -1,8 +1,18 @@
 
-#' constants for validation
+#' biomass columns
+#'
+#' @export
 biomass_column_names <- c("spp_who", "spp_date", "ID_new", "source", "external_trt", "trt_type", "reponse_var", "site", "spp_biomass", "f_spp_name", "f_nat_inv", "f_.indiv", "c_spp_name", "c_nat_inv", "c_.indiv", "biomass_type", "response_mean", "response_transformation", "response_mean_unit", "response_var", "response_var_unit", "sample_size", "nutrient_general", "nutrient_compound", "nutrient1_mean", "nutrient2_mean", "nutrient3_mean", "nutrient_unit", "nutrient_interval_days", "nutrient_addition_duration", "ext_treatment_mean", "ext_treatment_unit", "notes")
-biomass_validation_file <- 'inst/biomass_validation_rules.yaml'
-env_validation_file <- 'int/env_validation_rules.yml'
+
+# #' default file with validation rules for biomass data
+# #'
+# #' @export
+# biomass_validation_file <- system.file('rules', 'biomass_validation_rules.yaml', package='commruleR')
+
+# #' default file with validation rules for env data
+# #'
+# #' @export
+# env_validation_file <- system.file('rules', 'env_validation_rules.yaml', package='commruleR')
 
 
 #' read in one tab (only) of  meta-data analysis google sheet
@@ -31,7 +41,10 @@ validate_biomass_columns<- function(biomass_data){
 #' use the validate package to check a file against the
 #'
 #' @export
-validate_biomass_data<- function(biomass_data, validation_file = biomass_validation_file ){
+validate_biomass_data<- function(biomass_data, validation_file = NULL ){
+  if(is.null(validation_file)) {
+    validation_file = system.file('rules', 'biomass_validation_rules.yaml', package='commruleR')
+  }
   biomass_rules <- validate::validator(.file = validation_file)
   validation_results <- validate::confront(biomass_data, biomass_rules)
   summary(validation_results)
