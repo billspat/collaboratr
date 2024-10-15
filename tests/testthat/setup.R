aab20fb56746141e0ce95189811623706f92f620
Pat Bills <billspat@msu.edu>
2024-10-15 15:24:07 -0400

    tests of reading lists and validation

---
+++ tests/testthat/setup.R
@@ -0,0 +1,1 @@
+test_biomass_url<- 'https://docs.google.com/spreadsheets/d/1JVTRm1uQCrcOjXCULBYEgbW5oyd1S7SVGq6D1JpwCW4/edit?gid=0#gid=0'
--- tests/testthat/test_basic_validation.R
+++ tests/testthat/test_basic_validation.R
@@ -4,7 +4,6 @@ test_that("tests work", {
   expect_true(TRUE)
 })
 
-
 test_that("can read biomass", {
   # check this
   test_biomass_url<- 'https://docs.google.com/spreadsheets/d/1JVTRm1uQCrcOjXCULBYEgbW5oyd1S7SVGq6D1JpwCW4/edit?gid=0#gid=0'
@@ -15,4 +14,14 @@ test_that("can read biomass", {
 
 })
 
+test_that("can read biomass rules", {
+  yaml_file <- "../../inst/rules/biomass_validation_rules.yaml"
+  expect_true(file.exists(yaml_file))
+  validation_rules <- validate::validator(.file= yaml_file)
+  # in setup test_biomass_url
+  df <- read_gsheet_by_url(gurl = test_biomass_url, sheet_id = 1, has_description_line = TRUE)
+  c <- validate::confront(df, validation_rules)
+  expect_equal( class(c)[1],  "validation")
 
+}
+)
