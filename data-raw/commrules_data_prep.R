3ad4e33f91c358b6054af9eaffc0b7128d8e3833
Pat Bills <billspat@msu.edu>
2024-11-01 15:55:36 -0400

    updated column defintions from google sheets

--- data-raw/commrules_data_prep.R
+++ data-raw/commrules_data_prep.R
@@ -2,10 +2,9 @@
 
 raw_data_dir = 'data-raw'
 
+# data column specifications
 commassembly_rules_template <- Sys.getenv('TEST_TEMPLATE_URL')
-
 commassembly_rules_biomass_str <-  googlesheets4::read_sheet(googledrive::as_id(commassembly_rules_template), sheet = 'biomass_str')
-#   readr::read_csv(file.path(raw_data_dir, 'biomass_spec.csv'))
 usethis::use_data(commassembly_rules_biomass_str, overwrite = TRUE)
 commassembly_rules_env_str <-  googlesheets4::read_sheet(googledrive::as_id(commassembly_rules_template), sheet = 'env_str')
 usethis::use_data(commassembly_rules_env_str, overwrite = TRUE)
