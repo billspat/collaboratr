406725476d65f3f47e11f807e74cd4c98336afd8
Pat Bills <billspat@msu.edu>
2024-11-01 15:56:04 -0400

    new workflow script for validating all sheets

--- R/validator.R
+++ R/validator.R
@@ -1,99 +1,141 @@
-# ### NOTE : this is the initial version of this script,
-# # which is specific to one project, not a generic package.
+## MSU IBEEM : Community Assembly Rules project
+## Validation workflow functions
+
+# this R project is constructed as an R package, but this particular script
+# does not have any exported functions like a package would.  This is for
+# 1) it's still under development
+# 2) it's very specific to the data for the Community Assembly Rules project
+
+# ultimately this will be written as part of a vignette or sample code that uses
+# the validation rules and column definitions for this project as examples
+
+# to use these functions
+#  1. ensure that you have permissions in GCP and keys in .Renviron
+#    in .Renviron also set the URL for the google sheet with the list of sheet
+#    urls to validate
+#  2. ensure you have the most up-to-date versions of the data template
+#    run the script data-raw/commrules_data_prep.R to generate new data/*.rda
+#  3. source this file
+#    that will automatically run the google authentication setup, so you
+#    may have to make a choice of using an existing auth, or logging in with
+#    via web
+#   it also auto runs devtools::load_all()
+#   to load all the other functions from the project, which also loads the data
+#  4. run the function validate_all() from the R console which reads all
+
+require(dplyr)
+devtools::load_all()
+gsheet_auth_setup(drive_email = drive_email)
+
+### GLOBALS
+csv_folder = '../L0'
+dir.create(csv_folder, showWarnings = FALSE)
+biomass_validation_file <- 'inst/rules/biomass_validation_rules.yaml'
+env_validation_file <- 'inst/rules/env_validation_rules.yaml'
+data(commassembly_rules_biomass_str)
+data(commassembly_rules_env_str)
 
 
-# #' Column Definitions for CommecologyRULEs biomass datasets
-# #'
-# #' list of column names with meanings, data type, units and examples
-# #'
-# #' @format ## `commecology_rules_biomass_str`
-# #' A data frame 40-ish rows and 4 columns:
-# #' \describe{
-# #'   \item{col_name}{name of column in biomass}       ""
-# #'   \item{col_description}{desc}
-# #'   \item{col_type}{R data type}
-# #'   \item{example}{what may go in the data}
-# #' }
-# "commecology_rules_biomass_str"
+# sheet_data is one row of the URLS data frame, as a list, url is ..$url
+read_and_report<- function(url, tab_name, spec.df){
+  read_data_sheet_save_warnings <- commruleR::errorSaver(read_data_sheet)
+  data.df <- read_data_sheet_save_warnings(gurl = url,
+                                           tab_name,
+                                           spec.df)
+
+  if("warnings" %in% names(data.df)) {
+    print(paste("reading warnings for tab_name:", tab_name, "url", url))
+    print(data.df$warnings)
+    # get just the data so what's left can be validated
+    data.df <- data.df[[1]]
+  }
+
+  return(data.df)
+}
 
 
-
-# #' validation rules for biomass data
-# #'
-# "biomass_validator"
-
-
-# biomass_validation_file <- system.file('rules', 'biomass_validation_rules.yaml', package='commruleR')
-
-# # #' default file with validation rules for env data
-# # #'
-# # #' @export
-# # env_validation_file <- system.file('rules', 'env_validation_rules.yaml', package='commruleR')
-
-
-# # #' read in two data tabs (only) of  meta-data analysis google sheet
-# # #'
-# # #' @param sheeturl the url of of the google sheet that has commecologyRULES data
-# # #' @param has_description_line T/F datasheets use first row with explanatory text in google sheet, remove if TRUE.  defaults to TRUE
-# # #' @param drive_email the email to use for google drive log-in, which is the institution that setup the project
-# # #'
-# #' @returns list of data frames, on for each google sheet tab
-# #' @export
-# read_commrules_sheet<-function(sheet_url, has_description_line = TRUE, drive_email=NULL){
-#   # gdrive_setup(drive_email = drive_email)
-
-#   biomass_df <- read_gsheet_by_url(gurl = sheet_url, sheet_id = 1, has_description_line = has_description_line )
-#   env_df <- read_gsheet_by_url(gurl = sheet_url, sheet_id = 2, has_description_line = has_description_line)
-
-#   return(list('biomass' = biomass_df, 'env' = env_df))
-# }
-
-# # #' validate columns present for biomass data
-# # #'
-# # #' @param biomass_data data frame of biomass data from commecologyRules
-# validate_biomass_columns<- function(biomass_data){
-#   return(  identical(sort(commecology_rules_biomass_str$col_name), sort(names(biomass_data))) )
-# }
+validation_report<- function(data.df, validation_file){
+  confrontation<- validate_from_file(data.df, validation_file)
+  validation_summary <- validate::summary(confrontation)
+  if(sum(validation_summary$fails) == 0) {
+    return(TRUE)
+  } else {
+    # print(validation_summary)
+    fails <- dplyr::filter(validation_summary , fails > 0)
+    print(fails)
+    return(FALSE)
+  }
+}
 
 
-# # #' validate biomass data
-# # #'
-# # #' use the validate package to check a file against the
-# # #' @param biomass_data dataframe of biomass data
-# # #' @param validation_file optional file with validation rules in it.
-# # #' @export
-# validate_biomass_data<- function(biomass_data, validation_file = NULL ){
-#   if(is.null(validation_file)) {
-#     validation_file = system.file('rules', 'biomass_validation_rules.yaml', package='commruleR')
-#   }
-#   biomass_rules <- validate::validator(.file = validation_file)
-#   validation_results <- validate::confront(biomass_data, biomass_rules)
-#   validate::summary(validation_results)
-#   return(validation_results)
+save_csvs<- function(sheet_data, verbose = FALSE){
 
-# }
+  if(verbose == TRUE) print(paste(sheet_data$id, sheet_data$url))
 
-# # #' validate env columns
-# # #'
-# # #' check that all columns present in imported env data
-# # #' @param env_data data frame from imported env spreadsheet
-# # #' @export
-# validate_env_columns<- function(env_data){
-#   env_columns <- c('env_who','env_date','ID_new','exp_type','biome_type','country','state_city_province','latitude','longitude','coord_units','site','rainshelter','avg_rep_temp_C','avg_rep_ppt_mm','avg_rep_elev_m','year_start','year_end','exp_duration_days','notes')
-#   return(  identical(sort(env_columns), sort(names(env_data))) )
-# }
+  url <- sheet_data$url
 
-# # #' run all validation checks for biomass
-# #'
-# # #' @param biomass_df data frame with biomass data
-# # #' @param validation_file optional file to read for validation, will read from inst/rules if not sent
-# # #' @returns boolean TRUE if all validation checks pass
-# validate_biomass<- function(biomass_df, validation_file = NULL){
-#   if(! validate_biomass_columns(biomass_df))
-#     warning("Columns in sheet don't match expected list of columns")
-#     return(FALSE)
+  #### ENV
+  env.df <- read_and_report(url,
+                            tab_name = 'env_data',
+                            spec.df = commassembly_rules_env_str)
+  if(!exists('env.df')) {
+    print(paste("could not read sheet: "))
+    return (c(NA, NA))
+  }
+  env_valid <- validation_report(env.df, env_validation_file)
 
-#   r <- validate_biomass_data(biomass_data = biomass_df, validation_file = validation_file)
-#   return(r)
+  ## BIOMASS READ
+  biomass.df <- read_and_report(url,
+                                tab_name = 'biomass_data',
+                                spec.df = commassembly_rules_biomass_str)
+  if(!exists('biomass.df')) {
+    print(paste("could not read sheet "))
+    return (c(NA, NA))
+  }
+  # BIOMASS VALIDATION
+  biomass_valid <- validation_report(biomass.df, biomass_validation_file)
 
-# }
+  if(! (biomass_valid && env_valid)) {
+    print(paste(sheet_data$id, sheet_data$url, " validated: biomass=", biomass_valid, " env=", env_valid ))
+  }
+  # SAVE
+  id_new <- sheet_data$ID_new
+  biomass_file_name <- file.path(csv_folder, paste0('biomass_', id_new, '.csv'))
+  write.csv(biomass.df, biomass_file_name, row.names = FALSE)
+  env_file_name <- file.path(csv_folder, paste0('env_', id_new, '.csv'))
+  write.csv(env.df, env_file_name, row.names = FALSE)
+  return(c(biomass_file_name, env_file_name))
+
+}
+
+validate_one<-function(id, urls.df = NULL, verbose = FALSE){
+  if(is.null(urls.df)){
+    doc_with_list_url <- Sys.getenv('TEST_ID_LIST_URL')
+    id_column = 'ID_new'
+    urls.df <- read_url_list(gurl = doc_with_list_url, id_column = id_column, url_column='url')
+  }
+
+
+  study_info <- as.list(urls.df[urls.df$id == id,])
+  if(verbose ==TRUE) print(paste(study_info$id, study_info$url))
+
+  tryCatch({
+    file_names<- save_csvs(study_info)
+    print(file_names)
+  }, error=function(e) print(e))
+
+}
+
+
+validate_all<- function(){
+
+  # list of URLS from google drive to a data sheet
+  doc_with_list_url <- Sys.getenv('TEST_ID_LIST_URL')
+  id_column = 'ID_new'
+  urls.df <- read_url_list(gurl = doc_with_list_url, id_column = id_column, url_column='url')
+
+
+  for(i in seq(1:nrow(urls.df))) {
+    validate_one(id = urls.df$id[i], urls.df)
+  }
+}
